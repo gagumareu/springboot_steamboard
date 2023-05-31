f5675c2b814d65d57197649325cdcef09b468f82
intteliHome <iseoulyou@gmail.com>
2023-05-31 22:06:21 +0900

    first

---
+++ src/main/java/org/coke/SteamboardApplication.java
@@ -0,0 +1,13 @@
+package org.coke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SteamboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SteamboardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/coke/SteamboardApplicationTests.java
@@ -0,0 +1,13 @@
+package org.coke;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SteamboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
